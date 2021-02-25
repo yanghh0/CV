4ae008e48379f2c9bb3f9480a100a2b87061882c
yanghh0 <13588036019@163.com>
2021-02-25 17:42:33 +0800

    Update voc0712.py

--- SSD/data/voc0712.py
+++ SSD/data/voc0712.py
@@ -18,7 +18,7 @@ if sys.version_info[0] == 2:
 else:
     import xml.etree.ElementTree as ET
 
-# 20 个物体种类，always index 0
+# VOC的数据类别有 20 种，always index 0
 VOC_CLASSES = (
     'aeroplane', 
     'bicycle', 
@@ -52,26 +52,40 @@ class VOCAnnotationTransform(object):
     Arguments:
         class_to_ind (dict, optional): dictionary lookup of classnames -> indexes
             (default: alphabetic indexing of VOC's 20 classes)
-        keep_difficult (bool, optional): keep difficult instances or not
-            (default: False)
+        keep_difficult (bool, optional): keep difficult instances or not (default: False)
         height (int): height
         width (int): width
+
+    解释：
+        将VOC的annotation中的bbox坐标转化为归一化的值；
+        将类别转化为用索引来表示的字典形式；
+        参数列表：
+            class_to_ind: 类别的索引字典。
+            keep_difficult: 是否保留difficult=1的物体。
     """
 
     def __init__(self, class_to_ind=None, keep_difficult=False):
-        self.class_to_ind = class_to_ind or dict(
-            zip(VOC_CLASSES, range(len(VOC_CLASSES))))
+        self.class_to_ind = class_to_ind or dict(zip(VOC_CLASSES, range(len(VOC_CLASSES))))
         self.keep_difficult = keep_difficult
 
     def __call__(self, target, width, height):
         """
         Arguments:
-            target (annotation) : the target annotation to be made usable
-                will be an ET.Element
+            target (annotation) : the target annotation to be made usable will be an ET.Element
         Returns:
             a list containing lists of bounding boxes  [bbox coords, class name]
+
+        解释：
+        参数列表：
+            target: xml被读取的一个ET.Element对象
+            width: 图片宽度
+            height: 图片高度
+        返回值：
+            一个list，list中的每个元素是[bbox coords, class name]
+
         """
         res = []
+        # 遍历 xml 文件中所有的 object
         for obj in target.iter('object'):
             difficult = int(obj.find('difficult').text) == 1
             if not self.keep_difficult and difficult:
@@ -83,9 +97,10 @@ class VOCAnnotationTransform(object):
             bndbox = []
             for i, pt in enumerate(pts):
                 cur_pt = int(bbox.find(pt).text) - 1
-                # scale height or width
+                # scale height or width，即归一化，x/w，y/h
                 cur_pt = cur_pt / width if i % 2 == 0 else cur_pt / height
                 bndbox.append(cur_pt)
+            # 提取类别名称对应的 index
             label_idx = self.class_to_ind[name]
             bndbox.append(label_idx)
             res += [bndbox]  # [xmin, ymin, xmax, ymax, label_ind]
@@ -105,7 +120,10 @@ class VOCDetection(data.Dataset):
         transform (callable, optional): transformation to perform on the input image
         target_transform (callable, optional): transformation to perform on the target `annotation`
             (eg: take in caption string, return tensor of word indices)
-        dataset_name (string, optional): which dataset to load(default: 'VOC2007')
+        dataset_name (string, optional): which dataset to load (default: 'VOC2007')
+
+    解释：
+        根据VOCAnnotationTransform()和VOC数据集的文件结构，读取图片，bbox和lable，构建VOC数据集。
     """
 
     def __init__(self, root,
@@ -120,6 +138,7 @@ class VOCDetection(data.Dataset):
         self._annopath = osp.join('%s', 'Annotations', '%s.xml')
         self._imgpath = osp.join('%s', 'JPEGImages', '%s.jpg')
         self.ids = list()
+
         for (year, name) in image_sets:
             rootpath = osp.join(self.root, 'VOC' + year)
             for line in open(osp.join(rootpath, 'ImageSets', 'Main', name + '.txt')):
@@ -127,7 +146,6 @@ class VOCDetection(data.Dataset):
 
     def __getitem__(self, index):
         im, gt, h, w = self.pull_item(index)
-
         return im, gt
 
     def __len__(self):
@@ -140,13 +158,14 @@ class VOCDetection(data.Dataset):
         img = cv2.imread(self._imgpath % img_id)
         height, width, channels = img.shape
 
+        # 对xml执行VOCAnnotationTransform()操作
         if self.target_transform is not None:
             target = self.target_transform(target, width, height)
 
         if self.transform is not None:
             target = np.array(target)
             img, boxes, labels = self.transform(img, target[:, :4], target[:, 4])
-            # to rgb
+            # bgr to rgb
             img = img[:, :, (2, 1, 0)]
             # img = img.transpose(2, 0, 1)
             target = np.hstack((boxes, np.expand_dims(labels, axis=1)))
