f6adcfa0327e5ed1ad05927b005f56b00db4c245
yanghh0 <13588036019@163.com>
2021-02-25 15:22:12 +0800

    Update config.py

--- SSD/data/config.py
+++ SSD/data/config.py
@@ -2,7 +2,7 @@
 import os.path
 
 # gets home dir cross platform
-HOME = os.path.expanduser("~")
+# HOME = os.path.expanduser("~")
 
 # for making bounding boxes pretty
 COLORS = ((255, 0, 0, 128), (0, 255, 0, 128), (0, 0, 255, 128),
@@ -24,19 +24,4 @@ voc = {
     'variance': [0.1, 0.2],
     'clip': True,
     'name': 'VOC',
-}
-
-coco = {
-    'num_classes': 201,
-    'lr_steps': (280000, 360000, 400000),
-    'max_iter': 400000,
-    'feature_maps': [38, 19, 10, 5, 3, 1],
-    'min_dim': 300,
-    'steps': [8, 16, 32, 64, 100, 300],
-    'min_sizes': [21, 45, 99, 153, 207, 261],
-    'max_sizes': [45, 99, 153, 207, 261, 315],
-    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
-    'variance': [0.1, 0.2],
-    'clip': True,
-    'name': 'COCO',
 }
