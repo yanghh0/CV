8553dc09d8e1149a6945e2f9130a568bc542e64c
yanghh0 <13588036019@163.com>
2021-03-02 15:54:41 +0800

    Update __init__.py

--- SSD/layers/modules/__init__.py
+++ SSD/layers/modules/__init__.py
@@ -1,4 +1,4 @@
-from .l2norm import L2Norm
-from .multibox_loss import MultiBoxLoss
+from layers.modules.l2norm import L2Norm
+from layers.modules.multibox_loss import MultiBoxLoss
 
 __all__ = ['L2Norm', 'MultiBoxLoss']
