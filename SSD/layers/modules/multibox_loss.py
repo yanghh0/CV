bfac5fb40625e7c1ce945580fc6f5d86d9116e40
yanghh0 <13588036019@163.com>
2021-03-07 13:51:47 +0800

    Update multibox_loss.py

--- SSD/layers/modules/multibox_loss.py
+++ SSD/layers/modules/multibox_loss.py
@@ -2,9 +2,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
-from data import coco as cfg
-from ..box_utils import match, log_sum_exp
+from data import voc as cfg
+from layers.box_utils import match, log_sum_exp
 
 
 class MultiBoxLoss(nn.Module):
@@ -37,7 +36,7 @@ class MultiBoxLoss(nn.Module):
         self.use_gpu = use_gpu
         self.num_classes = num_classes
         self.threshold = overlap_thresh
-        self.background_label = bkg_label
+        self.background_label = bkg_label    # 背景类对应的label编号，默认0
         self.encode_target = encode_target
         self.use_prior_for_matching = prior_for_matching
         self.do_neg_mining = neg_mining
@@ -57,61 +56,97 @@ class MultiBoxLoss(nn.Module):
             targets (tensor): Ground truth boxes and labels for a batch,
                 shape: [batch_size,num_objs,5] (last idx is the label).
         """
+
+        # loc_data : (batch_size, 8732, 4)
+        # conf_data: (batch_size, 8732, 21)
+        # priors   : (8732, 4)
         loc_data, conf_data, priors = predictions
+
+        # num 即 batch_size
         num = loc_data.size(0)
+
+        # 这一句感觉没啥用
         priors = priors[:loc_data.size(1), :]
+
+        # num_priors = 8732
         num_priors = (priors.size(0))
+
+        # num_classes = 21
         num_classes = self.num_classes
 
         # match priors (default boxes) and ground truth boxes
         loc_t = torch.Tensor(num, num_priors, 4)
         conf_t = torch.LongTensor(num, num_priors)
+
         for idx in range(num):
-            truths = targets[idx][:, :-1].data
-            labels = targets[idx][:, -1].data
-            defaults = priors.data
-            match(self.threshold, truths, defaults, self.variance, labels,
-                  loc_t, conf_t, idx)
+            truths = targets[idx][:, :-1].data   # shape: (num_objs, 4)
+            labels = targets[idx][:, -1].data    # shape: (num_objs)
+            defaults = priors.data               # shape: (8732, 4)
+            match(self.threshold, truths, defaults, self.variance, labels, loc_t, conf_t, idx)
+
         if self.use_gpu:
             loc_t = loc_t.cuda()
             conf_t = conf_t.cuda()
-        # wrap targets
-        loc_t = Variable(loc_t, requires_grad=False)
-        conf_t = Variable(conf_t, requires_grad=False)
 
-        pos = conf_t > 0
-        num_pos = pos.sum(dim=1, keepdim=True)
+        #===========================================================================
+        # 计算所有正样本的定位损失,负样本不需要定位损失
+        #===========================================================================
 
-        # Localization Loss (Smooth L1)
-        # Shape: [batch,num_priors,4]
+        # 计算正样本的数量
+        pos = conf_t > 0                        # shape: (batch_size, 8732)
+        num_pos = pos.sum(dim=1, keepdim=True)  # shape: (batch_size, 1)
+
+        # Shape: [batch_size, 8732, 4] 正样本的索引
         pos_idx = pos.unsqueeze(pos.dim()).expand_as(loc_data)
+
+        # 正样本的定位误差预测值 shape: (一个 batch 的正样本数量, 4)
         loc_p = loc_data[pos_idx].view(-1, 4)
+
+        # 正样本的定位误差真实值 shape: (一个 batch 的正样本数量, 4)
         loc_t = loc_t[pos_idx].view(-1, 4)
+
+        # 所有正样本的定位损失
         loss_l = F.smooth_l1_loss(loc_p, loc_t, size_average=False)
 
+        #===========================================================================
+        # 对于类别损失，进行难样本挖掘，一张图片正负样本的比例控制为1:3
+        #===========================================================================
+
         # Compute max conf across batch for hard negative mining
-        batch_conf = conf_data.view(-1, self.num_classes)
+        batch_conf = conf_data.view(-1, self.num_classes)  # shape: (batch_size * num_priors, num_classes)
+
+        # 计算类别损失.每一个的log(sum(exp(21个的预测))) - 对应的真正预测值
+        # shape: (batch_size * num_priors, 1)
         loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t.view(-1, 1))
 
         # Hard Negative Mining
-        loss_c[pos] = 0  # filter out pos boxes for now
-        loss_c = loss_c.view(num, -1)
+        loss_c = loss_c.view(num, -1)   # shape (batch_size, num_priors)
+        loss_c[pos] = 0                 # 过滤掉正样本
+
         _, loss_idx = loss_c.sort(1, descending=True)
         _, idx_rank = loss_idx.sort(1)
-        num_pos = pos.long().sum(1, keepdim=True)
-        num_neg = torch.clamp(self.negpos_ratio*num_pos, max=pos.size(1)-1)
-        neg = idx_rank < num_neg.expand_as(idx_rank)
+        num_pos = pos.long().sum(1, keepdim=True)   # shape: (batch_size, 1)
+
+        num_neg = torch.clamp(self.negpos_ratio * num_pos, max=pos.size(1) - 1)
+        neg = idx_rank < num_neg.expand_as(idx_rank) # shape (batch_size, num_priors)
+
+        #===========================================================================
+        # 计算正负样本的类别损失
+        #===========================================================================
 
         # Confidence Loss Including Positive and Negative Examples
+        # 都扩展为[batch_size, 8732, 21]
         pos_idx = pos.unsqueeze(2).expand_as(conf_data)
         neg_idx = neg.unsqueeze(2).expand_as(conf_data)
-        conf_p = conf_data[(pos_idx+neg_idx).gt(0)].view(-1, self.num_classes)
-        targets_weighted = conf_t[(pos+neg).gt(0)]
+
+        # shape: (一个 batch 正样本数量 + 负样本数量, num_classes)
+        conf_p = conf_data[(pos_idx + neg_idx).gt(0)].view(-1, self.num_classes)
+        # shape: (一个 batch 正样本数量 + 负样本数量)
+        targets_weighted = conf_t[(pos + neg).gt(0)]
+
         loss_c = F.cross_entropy(conf_p, targets_weighted, size_average=False)
 
-        # Sum of losses: L(x,c,l,g) = (Lconf(x, c) + αLloc(x,l,g)) / N
-
-        N = num_pos.data.sum()
+        N = num_pos.data.sum().type('torch.cuda.FloatTensor')
         loss_l /= N
         loss_c /= N
         return loss_l, loss_c
