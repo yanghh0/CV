08cecaccd202da977d2267ae21cb60c3c5c3acd6
yanghh0 <13588036019@163.com>
2021-03-08 13:34:56 +0800

    Update detection.py

--- SSD/layers/functions/detection.py
+++ SSD/layers/functions/detection.py
@@ -3,18 +3,18 @@ from torch.autograd import Function
 from layers.box_utils import decode, nms
 from data import voc as cfg
 
-
 class Detect(Function):
-    """At test time, Detect is the final layer of SSD.  Decode location preds,
-    apply non-maximum suppression to location predictions based on conf
-    scores and threshold to a top_k number of output predictions for both
-    confidence score and locations.
-
-    detection层是ssd预测阶段的最后一层。
-    它接收底层网络输出的位置偏移量（loc_data）、各个框置信度（conf_data）以及默认框（prior_data）。
-    该层的作用是整合各层的预测结果，过滤置信度太低的预测框，通过类内nms抑制大量相同的预测框。
-    """
-    def __init__(self, num_classes, bkg_label, top_k, conf_thresh, nms_thresh):
+    @staticmethod
+    def forward(self, num_classes, bkg_label, top_k, conf_thresh, nms_thresh, loc_data, conf_data, prior_data):
+        """
+        Args:
+            loc_data: (tensor) Loc preds from loc layers
+                Shape: [batch,num_priors*4]
+            conf_data: (tensor) Shape: Conf preds from conf layers
+                Shape: [batch*num_priors,num_classes]
+            prior_data: (tensor) Prior boxes and variances from priorbox layers
+                Shape: [1,num_priors,4]
+        """
         self.num_classes = num_classes
         self.background_label = bkg_label
         self.top_k = top_k
@@ -25,30 +25,18 @@ class Detect(Function):
         self.conf_thresh = conf_thresh
         self.variance = cfg['variance']
 
-    def forward(self, loc_data, conf_data, prior_data):
-        """
-        Args:
-            loc_data: (tensor) Loc preds from loc layers
-                Shape: [batch,num_priors*4]
-            conf_data: (tensor) Shape: Conf preds from conf layers
-                Shape: [batch*num_priors,num_classes]
-            prior_data: (tensor) Prior boxes and variances from priorbox layers
-                Shape: [num_priors,4]
-        """
-        num = loc_data.size(0)  # batch size，就是一个batch内的图片个数
-        num_priors = prior_data.size(0)  # 先验框个数
+        num = loc_data.size(0)  # batch size
+        num_priors = prior_data.size(0)
         output = torch.zeros(num, self.num_classes, self.top_k, 5)
         conf_preds = conf_data.view(num, num_priors, self.num_classes).transpose(2, 1)
-
+        
         # Decode predictions into bboxes.
-        for i in range(num):  # 对每一张图片进行处理
-            # 对先验框解码获得预测框
+        for i in range(num):
             decoded_boxes = decode(loc_data[i], prior_data, self.variance)
             # For each class, perform nms
             conf_scores = conf_preds[i].clone()
 
             for cl in range(1, self.num_classes):
-                # 对每一类进行非极大抑制
                 c_mask = conf_scores[cl].gt(self.conf_thresh)
                 scores = conf_scores[cl][c_mask]
                 if scores.size(0) == 0:
@@ -63,3 +51,4 @@ class Detect(Function):
         _, rank = idx.sort(1)
         flt[(rank < self.top_k).unsqueeze(-1).expand_as(flt)].fill_(0)
         return output
+
